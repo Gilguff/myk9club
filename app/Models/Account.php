bdec33bc098764dbaee4ef66890a78ba17b4b44c
David Macario <david.macariopro@gmail.com>
2025-11-25 00:01:41 +0100

    update Account model

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -2,11 +2,23 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Account extends Model
 {
     /** @use HasFactory<\Database\Factories\AccountFactory> */
-    use HasFactory;
+    use HasFactory, HasUuids, SoftDeletes;
+
+    /**
+     * @return BelongsTo<User,Account>
+     */
+    public function owner(): HasOne
+    {
+        return $this->belongsTo(User::class, 'owner_id');
+    }
 }
--- database/migrations/2025_11_24_223822_create_accounts_table.php
+++ database/migrations/2025_11_24_223822_create_accounts_table.php
@@ -14,6 +14,10 @@ return new class extends Migration
         Schema::create('accounts', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('name');
+            $table->string('slug')->unique()->default(str()->random(8));
+            $table->foreignId('owner_id')->constrained('users')->onDelete('cascade');
+            $table->softDeletes();
         });
     }
 
