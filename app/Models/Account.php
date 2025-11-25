7e2a1bc238c3e9f0c480803ed2d1b540da73917c
David Macario <david.macariopro@gmail.com>
2025-11-25 03:12:59 +0100

    add users method to Account model

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -20,6 +20,17 @@ class Account extends Model
     public function owner(): HasOne
     {
         return $this->belongsTo(User::class, 'owner_id');
+    }
+
+    /**
+     * @return BelongsToMany<User,Account>
+     */
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class)
+            ->using(AccountUser::class)
+            ->withPivot('role')
+            ->withTimestamps();
     }
 
     protected $fillable = [
