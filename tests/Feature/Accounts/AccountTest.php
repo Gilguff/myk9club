1001d91fa10cf53115df842cab03cc93a526e018
David Macario <david.macariopro@gmail.com>
2025-11-25 23:14:49 +0100

    add account tests

--- tests/Feature/Accounts/AccountTest.php
+++ tests/Feature/Accounts/AccountTest.php
@@ -2,18 +2,30 @@
 
 use App\Models\User;
 
-test('Default personal account gets created on user registration', function () {
+test('Account gets created on user creation', function () {
     $user = User::factory()->create();
-    echo 'User ID: '.$user->id.PHP_EOL;
-    echo $user->personal_account()->get()->first().PHP_EOL;
-
-    $this->assertDatabaseHas('accounts', [
-        'user_id' => $user->id,
-    ]);
 
     $this->assertDatabaseHas('account_user', [
         'user_id' => $user->id,
-        'account_id' => $user->personal_account()->id,
+        'account_id' => $user->personal_account->id,
         'role' => 'owner',
     ]);
+});
+
+test('User is assigned as owner of personal account', function () {
+    $user = User::factory()->create();
+
+    $this->assertEquals('owner', $user->personal_account->users()->first()->pivot->role);
+});
+
+test('Default account is set to personal account on user creation', function () {
+    $user = User::factory()->create();
+
+    $this->assertEquals($user->personal_account->id, $user->default_account_id);
+});
+
+test('Personal account is marked correctly', function () {
+    $user = User::factory()->create();
+
+    $this->assertTrue($user->personal_account->is_personal);
 });
